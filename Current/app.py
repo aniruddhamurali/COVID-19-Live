398a9eb90f555deaaac65019272014ee9da7e5fd
Aniruddha <amurali62@gatech.edu>
2020-04-04 19:04:07 -0400

    Added U.S. state map to website

--- Current/app.py
+++ Current/app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 def globe_map():
     return render_template("globe-page.html")
 
+@app.route("/globe_map")
+def globe_map_copy():
+    return render_template("globe-page.html")
+
     
 @app.route("/coronavirus_bubble_map")
 def coronavirus_bubble_map():
     return render_template("bubble-map-page.html")
+
+@app.route("/us_state_map")
+def us_state_map():
+    return render_template("us-state-map-page.html")
 
     
 if __name__ == "__main__":
