287606f5cc44caf87262ba1181d622531033d876
Aniruddha <amurali62@gatech.edu>
2020-04-04 16:32:39 -0400

    Corrected bubble map information

--- Current/coronavirus-cases-plots/bubble-map.py
+++ Current/coronavirus-cases-plots/bubble-map.py
@@ -14,9 +14,39 @@ iso3_codes = coco.convert(names=countries['Country/Region'].tolist(), to='ISO3')
 countries['ISO_Codes'] = iso3_codes
 countries = countries[countries['ISO_Codes'] != 'not found']
 
+# Create a new dataframe with our cleaned country list
+dates = countries['Date'].unique().tolist()
+countries_cleaned = countries.groupby('Country/Region')
+countries_grouped = list(countries_cleaned)
 
-fig = px.scatter_geo(countries, locations="ISO_Codes", hover_name="Country/Region", 
-                                size="Confirmed", size_max=50,
+
+columns = ['Date', 'Country/Region', 'Lat', 'Long', 'ISO_Codes', 'Confirmed']
+df = pd.DataFrame(columns=columns)
+
+
+index = 0
+for country in countries_grouped:
+    country_data = country[1]
+    eachDate = list(country_data.groupby("Date"))
+    
+    for date in eachDate:
+        date_data = date[1]
+        df.append(pd.Series(name=index))
+        
+        df.at[index, 'Date'] = date[0] #country_data['Date'].iloc[index % len(date_data)]
+        df.at[index, 'Country/Region'] = country_data['Country/Region'].iloc[index % len(date_data)]
+        df.at[index, 'Lat'] = country_data['Lat'].iloc[index % len(date_data)]
+        df.at[index, 'Long'] = country_data['Long'].iloc[index % len(date_data)]
+        df.at[index, 'ISO_Codes'] = country_data['ISO_Codes'].iloc[index % len(date_data)]
+        
+        cases = date_data['Confirmed'].sum()
+        df.at[index, 'Confirmed'] = cases
+
+        index += 1
+
+
+fig = px.scatter_geo(df, locations="ISO_Codes", hover_name="Country/Region", 
+                                size=df["Confirmed"].astype(int), size_max=50,
                                 animation_frame="Date", projection="natural earth")
-#fig.show()
+fig.show()
 py.plot(fig, validate=False, filename='global-bubble-map')
