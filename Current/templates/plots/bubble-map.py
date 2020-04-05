382272c7cb9c9e37e648c96ae1df30faab37a124
Aniruddha <amurali62@gatech.edu>
2020-04-04 21:29:53 -0400

    Moved plot programs

--- Current/templates/plots/bubble-map.py
+++ Current/templates/plots/bubble-map.py
@@ -47,6 +47,7 @@ for country in countries_grouped:
 
 fig = px.scatter_geo(df, locations="ISO_Codes", hover_name="Country/Region", 
                                 size=df["Confirmed"].astype(int), size_max=50,
-                                animation_frame="Date", projection="natural earth")
+                                animation_frame="Date", projection="natural earth",
+                                width=1200)
 fig.show()
 py.plot(fig, validate=False, filename='global-bubble-map')
