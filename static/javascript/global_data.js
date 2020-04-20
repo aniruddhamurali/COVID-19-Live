4dfd739838ccfe4136f1596e5a7be201d36ddbf8
Aniruddha <aniruddha.murali@gmail.com>
2020-04-19 21:27:42 -0400

    Global data table sorting

--- static/javascript/global_data.js
+++ static/javascript/global_data.js
@@ -32,7 +32,6 @@ function displayHTMLTable(results) {
     if (data[i]["Date"] === "") i -= 1;
     var row = data[i];
     d = row["Date"];
-    console.log(date);
 
     var table_data = '<table class="table table-bordered table-sm table-hover">';
     for (var count = 0; count < data.length; count++) {
@@ -51,7 +50,7 @@ function displayHTMLTable(results) {
             continue;
         } else {
             table_data += '<tr>';
-            // Exclude data and percent
+            // Exclude data since we only want to show most recent data
             for (var cell_count = 1; cell_count < cell_data.length; cell_count++) {
                 table_data += '<td>' + cell_data[cell_count] + '</td>';
             }
@@ -59,7 +58,23 @@ function displayHTMLTable(results) {
         }
     }
     table_data += '</table>';
-    console.log(table_data);
     $('#global_data').html(table_data);
+
+    
+    $('th').click(function(){
+        console.log("HEY");
+        var table = $(this).parents('table').eq(0)
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+        this.asc = !this.asc
+        if (!this.asc){rows = rows.reverse()}
+        for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+    })
+    function comparer(index) {
+        return function(a, b) {
+            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        }
+    }
+    function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
 }
 
