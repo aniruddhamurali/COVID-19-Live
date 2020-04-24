37a357ef7c290f2d9f5a69cac770814b1578a927
Aniruddha <aniruddha.murali@gmail.com>
2020-04-24 18:40:03 -0400

    Modified testing plots and saved testing plot as html

--- templates/plots/global-testing.py
+++ templates/plots/global-testing.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import plotly.offline as py
 
 import numpy as np
 import pandas as pd
@@ -13,22 +14,23 @@ df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/pu
 
 today = date.today()
 yesterday = today - timedelta(days = 1)
-#df = df.loc[df['Date'] == yesterday]
+
+df['Country'] = [e.split('-')[0] for e in df['Entity']]
 
 # Creating two subplots
 fig = make_subplots(rows = 1, cols = 2, 
                     specs = [[{}, {}]], 
                     shared_xaxes = True,
-                    shared_yaxes = False, 
+                    shared_yaxes = True, 
                     vertical_spacing = 0.001)
 
 fig.append_trace(go.Bar(
     x = df['Cumulative total per thousand'],
-    y = df['Entity'],
+    y = df['Country'],
     marker = dict(
-        color = 'rgba(50, 171, 96, 0.6)',
+        color = 'rgba(37, 167, 247, 0.7)',
         line = dict(
-            color = 'rgba(50, 171, 96, 1.0)',
+            color = 'rgba(37, 167, 247, 1.0)',
             width = 1),
     ),
     name = 'Cumulative total of COVID-19 tests done per 1 thousand people',
@@ -37,49 +39,52 @@ fig.append_trace(go.Bar(
 
 fig.append_trace(go.Scatter(
     x = df['Daily change in cumulative total per thousand'], 
-    y = df['Entity'],
+    y = df['Country'],
     mode = 'lines+markers',
-    line_color = 'rgb(128, 0, 128)',
+    line_color = 'rgb(245, 141, 66)',
     name = 'Daily change in cumulative total of COVID-19 tests done per 1 thousand people',
     connectgaps = True,
+    marker = dict(size = 7)
 ), 1, 2)
 
 fig.update_layout(
     title = 'Cumulative Totals and Daily Changes in COVID-19 Testing',
+    title_x = 0.5,
     yaxis = dict(
-        showgrid = False,
+        showgrid = True,
         showline = False,
         showticklabels = True,
-        domain = [0, 0.95],
+        domain = [0.1, 0.95],
     ),
     yaxis2 = dict(
-        showgrid = False,
+        showgrid = True,
         showline = True,
         showticklabels = False,
-        linecolor = 'rgba(102, 102, 102, 0.8)',
-        linewidth = 2,
-        domain = [0, 0.95],
+        domain = [0.1, 0.95],
     ),
     xaxis = dict(
         zeroline = False,
         showline = False,
         showticklabels = True,
         showgrid = True,
-        domain = [0, 0.42],
+        domain = [0.05, 0.43],
+        side = 'top',
+        dtick = 10,
+        fixedrange = True
     ),
     xaxis2 = dict(
         zeroline = False,
         showline = False,
         showticklabels = True,
         showgrid = True,
-        domain = [0.47, 1],
+        domain = [0.46, 0.95],
         side = 'top',
         dtick = 0.2,
+        fixedrange = True
     ),
-    legend = dict(x = 0.01, y = 1.01, font_size = 12),
-    margin = dict(l = 100, r = 20, t = 70, b = 70),
-    paper_bgcolor = 'rgb(248, 248, 255)',
-    plot_bgcolor = 'rgb(248, 248, 255)',
+    legend = dict(x = 0.32, y = 1.01, font_size = 12),
+    margin = dict(l = 100, r = 20, t = 70, b = 0),
+    template = "plotly_dark"
 )
 
 
@@ -111,15 +116,19 @@ for ydn, yd, xd in zip(delta_per, total_per, df['Entity']):
 
 # Note
 annotations.append(dict(xref = 'paper', yref = 'paper',
-                        x = -0.2, y = -0.109,
+                        x = 0.5, y = 0.06,
                         text='Note: Some countries do not have data on daily changes in the ' +
-                             'total number of people tested per thousand people',
+                             'total number of tests done per 1 thousand people and/or the cumulative ' +
+                             'total number of tests done per 1 thousand people. <br>' +
+                             'Some countries also had multiple sources stating different numbers of ' + 
+                             'tests, so all sources are included in these plots. Look at data table for more details.',
                         font = dict(family = 'Arial', 
-                                    size = 10, 
-                                    color = 'rgb(150,150,150)'),
+                                    size = 12, 
+                                    color = 'rgb(225,225,225)'),
                         showarrow = False))
 
 fig.update_layout(annotations = annotations,
-                  height=1400)
+                  height = 1700)
 
-fig.show()
+#fig.show()
+py.plot(fig, validate=False, filename='testing-plots')
