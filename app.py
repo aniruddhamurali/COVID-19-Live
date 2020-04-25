3dd3f6dcf5c1126ca371d8385ad469cb378820a6
Aniruddha <aniruddha.murali@gmail.com>
2020-04-24 21:03:41 -0400

    Added testing data page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def globe_map():
     return render_template("pages/globe-page.html")
 
+
 @app.route("/globe_map")
 def globe_map_copy():
     return render_template("pages/globe-page.html")
@@ -13,6 +14,11 @@ def globe_map_copy():
 @app.route("/coronavirus_bubble_map")
 def coronavirus_bubble_map():
     return render_template("pages/bubble-map-page.html")
+
+@app.route("/global_data")
+def global_data():
+    return render_template("pages/global-data-page.html")
+
 
 @app.route("/us_states_map")
 def us_states_map():
@@ -22,17 +28,18 @@ def us_states_map():
 def us_counties_map():
     return render_template("pages/us-counties-map-page.html")
 
-@app.route("/global_data")
-def global_data():
-    return render_template("pages/global-data-page.html")
-
 @app.route("/county_data")
 def county_data():
     return render_template("pages/county-data-page.html")
 
+
 @app.route("/global_testing")
 def global_testing():
     return render_template("pages/testing-plots-page.html")
+
+@app.route("/testing_data")
+def testing_data():
+    return render_template("pages/testing-data-page.html")
 
     
 if __name__ == "__main__":
