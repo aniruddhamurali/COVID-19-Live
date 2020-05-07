7958b9bfa36f023feef587b3224fc65aa5ea23b7
Aniruddha <aniruddha.murali@gmail.com>
2020-05-06 20:27:59 -0400

    Display testing center data on website

--- static/javascript/testing_centers.js
+++ static/javascript/testing_centers.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
     $.get("/get_testing_centers", function(data) {
         //console.log($.parseJSON(data))
-        console.log(data);
+        results = $.parseJSON(data);
+        var htmlResult = '';
+        for (var i = 0; i < results.length; i++) {
+            htmlResult += '<div style="width: 50%;>"'
+            name = results[i].name;
+            details = results[i].details;
+            appointment = results[i].appointment;
+            referral = results[i].referral;
+            testing = results[i].testing;
+            driving = results[i].drive_through;
+            instructions = results[i].instructions;
+            link = results[i].link;
+            //console.log(results[i]);
+            console.log(name);
+            htmlResult += '<h3>' + name + '</h3>' +
+                          '<h5>' + details + '</h5>' +
+                          '<p>' + appointment + '</p>' +
+                          '<p>' + referral + '</p>' +
+                          '<p>' + testing + '</p>' +
+                          '<p>' + driving + '</p>' +
+                          '<p>' + 'Instructions: ' + instructions + '</p>' +
+                          '</div>'
+        }
+        //console.log(data);
+        $('#testing_centers').html(htmlResult);
     });
 });
