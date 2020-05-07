a1475f726059292ed143fae9a2277c9ac72154c8
Aniruddha <aniruddha.murali@gmail.com>
2020-05-07 00:20:38 -0400

    Can now click through nearby testing centers

--- static/javascript/testing_centers.js
+++ static/javascript/testing_centers.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
+    var first = false;
+    var j = 0;
+    var cards = [];
+
     $.get("/get_testing_centers", function(data) {
         //console.log($.parseJSON(data))
         results = $.parseJSON(data);
         var htmlResult = '';
+        //var cards = [];
         for (var i = 0; i < results.length; i++) {
-            htmlResult += '<div style="width: 50%;>"'
+            htmlResult += '<a>'
             name = results[i].name;
             details = results[i].details;
             appointment = results[i].appointment;
@@ -13,18 +18,41 @@ $(document).ready(function () {
             driving = results[i].drive_through;
             instructions = results[i].instructions;
             link = results[i].link;
-            //console.log(results[i]);
-            console.log(name);
+
             htmlResult += '<h3>' + name + '</h3>' +
                           '<h5>' + details + '</h5>' +
                           '<p>' + appointment + '</p>' +
                           '<p>' + referral + '</p>' +
                           '<p>' + testing + '</p>' +
                           '<p>' + driving + '</p>' +
-                          '<p>' + 'Instructions: ' + instructions + '</p>' +
-                          '</div>'
+                          '<p>' + instructions + '</p>' +
+                          '</a>'
+
+            // new code
+            cards.push(htmlResult);
+            htmlResult = '';
         }
         //console.log(data);
-        $('#testing_centers').html(htmlResult);
+        //$('.scrollmenu').html(htmlResult);
+        console.log(cards);
+        if (first == false) {
+            $('#card').html(cards[0]);
+            first = true;
+            j = 1;
+        }
+    });
+
+    $("#right").click(function() {
+        console.log(j);
+        $('#card').html(cards[j]);
+        if (j == cards.length) j = 0;
+        else j += 1;    
+    });
+
+    $("#left").click(function() {
+        console.log(j);
+        $('#card').html(cards[j]);
+        if (j == 0) j = cards.length - 1;
+        else j -= 1;    
     });
 });
