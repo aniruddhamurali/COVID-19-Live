d57d3dca071cb136df33197e30c777d945df8e48
Aniruddha <aniruddha.murali@gmail.com>
2020-05-09 22:57:36 -0400

    Created actual model, visualized model as PNG

--- other_data/model.py
+++ other_data/model.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import OneHotEncoder
+from sklearn import tree
 
 import sys
 sys.path.append("../")
@@ -71,6 +72,13 @@ def get_mae(max_leaf_nodes):
     mae = mean_absolute_error(y_valid, preds_val)
     return(mae)
 
+X = pd.concat([OH_X_train, OH_X_valid], axis=0)
+y = pd.concat([y_train, y_valid], axis=0)
+
+model = DecisionTreeRegressor(max_leaf_nodes=500, random_state=0)
+model.fit(X, y)
+
+#tree.plot_tree(model)
 
 '''
 # compare MAE with differing values of max_leaf_nodes
@@ -79,3 +87,22 @@ for max_leaf_nodes in [5, 50, 500]:
     my_mae = get_mae(max_leaf_nodes)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
 '''
+
+
+
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+from sklearn.tree import export_graphviz
+import pydotplus
+
+dot_data = StringIO()
+
+export_graphviz(model, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,
+                feature_names=X.columns)
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+Image(graph.create_png())
+graph.write_png("model")
+
