1ea8b6fe38425d4db3afc797d2ea550b05a8e372
Aniruddha <aniruddha.murali@gmail.com>
2020-05-10 20:00:57 -0400

    Fixed decision tree model and visual

--- other_data/model.py
+++ other_data/model.py
@@ -6,7 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import OneHotEncoder
-from sklearn import tree
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO 
+
+from IPython.display import Image  
+import pydotplus
 
 import sys
 sys.path.append("../")
@@ -45,22 +49,54 @@ print(object_cols)
 OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
 OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
 OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
-print(OH_cols_valid.head())
+
+# Rename encoded columns
+names = OH_encoder.get_feature_names()
+for i in range(len(names)):
+    x = names[i].find('_')
+    names[i] = names[i][x+1:]
+    if i >= 3 and i <= 13: names[i] += "_age"
+    elif i >= 14 and i <= 22: names[i] += "_blood-type"
+    elif i == 23: names[i] = 'covid19 negative'
+    elif i == 24: names[i] = 'covid19 positive'
+    elif i == 25: names[i] = 'no covid19 symptoms'
+    elif i == 26: names[i] = 'has covid19 symptoms'
+    elif i == 27: names[i] = 'no covid19 contact'
+    elif i == 28: names[i] = 'had covid19 contact'
+    elif i == 29: names[i] = 'no asthma'
+    elif i == 30: names[i] = 'asthma'
+    elif i == 31: names[i] = 'no kidney disease'
+    elif i == 32: names[i] = 'kidney disease'
+    elif i == 33: names[i] = 'not immunocompromised'
+    elif i == 34: names[i] = 'immunocompromised'
+    elif i == 35: names[i] = 'no heart disease'
+    elif i == 36: names[i] = 'heart disease'
+    elif i == 37: names[i] = 'no lung disease'
+    elif i == 38: names[i] = 'lung disease'
+    elif i == 39: names[i] = 'no diabetes'
+    elif i == 40: names[i] = 'diabetes'
+    elif i == 41: names[i] = 'hiv negative'
+    elif i == 42: names[i] = 'hiv positive'
+    elif i == 43: names[i] = 'no hypertension'
+    elif i == 44: names[i] = 'hypertension'
+    elif i == 45: names[i] = 'no other chronic illnesses'
+    elif i == 46: names[i] = 'other chronic illnesses'
+
+print(names)
+OH_cols_train.columns = names
+OH_cols_valid.columns = names
 
 # One-hot encoding removed index; put it back
 OH_cols_train.index = X_train.index
 OH_cols_valid.index = X_valid.index
 
-
 # Remove categorical columns (will replace with one-hot encoding)
 num_X_train = X_train.drop(object_cols, axis=1)
 num_X_valid = X_valid.drop(object_cols, axis=1)
-print(num_X_valid.head())
 
 # Add one-hot encoded columns to numerical features
 OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
 OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
-print(OH_X_valid.head())
 
 def get_mae(max_leaf_nodes):
     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
@@ -78,8 +114,6 @@ y = pd.concat([y_train, y_valid], axis=0)
 model = DecisionTreeRegressor(max_leaf_nodes=500, random_state=0)
 model.fit(X, y)
 
-#tree.plot_tree(model)
-
 '''
 # compare MAE with differing values of max_leaf_nodes
 # 500 did very well
@@ -88,13 +122,7 @@ for max_leaf_nodes in [5, 50, 500]:
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
 '''
 
-
-
-from sklearn.externals.six import StringIO  
-from IPython.display import Image  
-from sklearn.tree import export_graphviz
-import pydotplus
-
+# Create decision tree visual
 dot_data = StringIO()
 
 export_graphviz(model, out_file=dot_data,  
@@ -104,5 +132,5 @@ export_graphviz(model, out_file=dot_data,
 
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
 Image(graph.create_png())
-graph.write_png("model")
+graph.write_png("model.png")
 
