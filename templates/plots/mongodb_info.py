e550856df1223548865ad4d6878c01b852994fbe
Aniruddha <aniruddha.murali@gmail.com>
2020-05-15 15:36:36 -0400

    Update mongodb info

--- templates/plots/mongodb_info.py
+++ templates/plots/mongodb_info.py
@@ -5,4 +5,4 @@ def getPassword():
     return 'An1ruddha'
 
 def getClient():
-    return "mongodb+srv://mainUser:An1ruddha@hospitalization-ptx4a.gcp.mongodb.net/test"
+    return "mongodb+srv://mainUser:An1ruddha@hospitalization-ptx4a.gcp.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE"
