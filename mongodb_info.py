9d28b0b7771e2f8082fae1b6dc83def814aa8d2a
Aniruddha <aniruddha.murali@gmail.com>
2020-05-15 11:13:03 -0400

    Modify gitignore

---
+++ mongodb_info.py
@@ -0,0 +1,8 @@
+def getUser():
+    return 'mainUser'
+
+def getPassword():
+    return 'An1ruddha'
+
+def getClient():
+    return "mongodb+srv://mainUser:An1ruddha@hospitalization-ptx4a.gcp.mongodb.net/test"
---
+++ static/javascript/keys.js
@@ -0,0 +1,4 @@
+var keys = {
+    news_key : 'b29d7e3e7b1c4f06b8a4e27c84c0f3a2',
+    fb_key : '2510767485690516'
+}
