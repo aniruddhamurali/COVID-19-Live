7675cc8bfd8e41ee3277493c59599266df2f75e2
Aniruddha <aniruddha.murali@gmail.com>
2020-05-18 15:28:57 -0400

    Testing centers page now uses user location

--- other_data/testing_centers.py
+++ other_data/testing_centers.py
@@ -1,16 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import pymongo
+import geocoder
+from geopy.geocoders import Nominatim
+
+
+# get current location
+def getCity():
+    g = geocoder.ip('me')
+    locator = Nominatim(user_agent="myGeocoder")
+    coordinates = str(g.latlng[0]) + ','  + str(g.latlng[1])
+    location = locator.reverse(coordinates)
+    return location.raw['address']['city'].lower()
 
 
 def run():
     # Desktop user-agent
     USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
     headers = {"user-agent" : USER_AGENT}
+    city = getCity()
 
     # Link to Google search for nearby COVID-19 testing centers
-    response = requests.get("https://www.google.com/search?rlz=1C5CHFA_enUS559US559&sxsrf=ALeKk02DeetRUkmRRjNC5Co7B7azzUtGRQ:1589204031549&q=covid+19+testing+centers&npsic=0&rflfq=1&rlha=0&rllag=41153517,-73314179,13078&tbm=lcl&ved=2ahUKEwjtrZqu9qvpAhU1ZDUKHZ-9A0oQjGp6BAgUED8&tbs=lrf:!3sIAE,lf:1,lf_ui:16&rldoc=1#rldoc=1&rlfi=hd:;si:,41.30159486477283,-71.626144996875;mv:[[41.8166219,-72.6467073],[41.00047730000001,-73.6158366]]",
-                            headers=headers)
+    response = requests.get("https://www.google.com/search?q=covid+19+testing+centers" + city + "&tbm=lcl&tbs=lrf:!3sIAE,lf:1,lf_ui:16&rldoc=1", headers=headers)
     response = response.text.strip()
     soup = BeautifulSoup(response, "html.parser")
 
