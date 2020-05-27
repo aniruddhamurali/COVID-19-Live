b408de0ca33cb7dc80415b9fd3dd9cbd73e9879d
Aniruddha <aniruddha.murali@gmail.com>
2020-05-27 10:53:57 -0400

    Remove dash sample code

--- templates/plots/us_state_map.py
+++ templates/plots/us_state_map.py
@@ -14,10 +14,6 @@ from datetime import timedelta
 import warnings
 warnings.filterwarnings('ignore')
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
 
 # Dict for US abbreviations
 us_state_abbrev = {
@@ -118,14 +114,6 @@ def run():
         template = "plotly_dark",
         dragmode = False,
     )
-
-    '''
-    app = dash.Dash()
-    app.layout = html.Div([
-        dcc.Graph(figure=fig)
-    ])
-    app.run_server(debug=True)
-    '''
 
     #py.plot(fig, config={"displayModeBar": False}, validate=False, auto_open=False)
     py.plot(fig, config={"displayModeBar": False}, validate=False, filename='./templates/plots/us-cases-map', auto_open=False)
